0a273421c9bec6f5b11d53cf2b3ca4c4682e9deb
katerynka <philipkatya13@gmail.com>
2026-05-20 13:11:25 +0300

    fix: remove OWNER and MANAGER write access to employee list

--- src/app/features/hr/employees/employee-list.component.ts
+++ src/app/features/hr/employees/employee-list.component.ts
@@ -47,7 +47,7 @@ export class EmployeeListComponent implements OnInit {
   ngOnInit(): void {
     this.currentUser$ = this.store.select(selectCurrentUser);
     this.store.select(selectCurrentUser).subscribe(user => {
-      this.canAdd = hasRole(user, 'MANAGER', 'HR', 'OWNER', 'IT_ADMIN');
+      this.canAdd = hasRole(user, 'HR', 'IT_ADMIN');
     });
   }
 
